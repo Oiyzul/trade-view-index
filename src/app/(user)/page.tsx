aed629069a2760a9e972bf6d6885ee8d2c387645
Waizul <waizul@gmail.com>
2025-10-04 01:25:47 +0600

    Implemented navbar

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen home-wrapper">trading updated view</div>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div>trading updated view</div>
-  )
-}
